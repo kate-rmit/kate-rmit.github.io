23e0361b5aaddd26ffb62f2d774f9ff0e8707c3f
kate-rmit <s4026131@rmit.edu.vn>
2025-10-28 16:28:57 +1100

    asm3

--- assignment 3/main.js
+++ assignment 3/main.js
@@ -16,8 +16,23 @@ function transform(section) {
     
 }
 
-// youtube tutorial: https://youtu.be/yXnK8ND76z8?si=Z2UHInG4Bw5pk3Jq
+// youtube tutorial for horizontal scrolling: https://youtu.be/yXnK8ND76z8?si=Z2UHInG4Bw5pk3Jq
+// youtube tutorial for adding music: https://www.youtube.com/watch?v=ZFqtk0Tsnsc
+// youtube tutorial for transition object: https://youtu.be/pVW3_5Pweqk?si=B8YJgHS62j8DqRNb
 
-window.addEventListener('click', () =>{
-  document.getElementById("song").play();
+
+// i found a some code on function stack that have a 
+// function of changing position fixed object to become sticky/stop at a certain point
+// https://jsfiddle.net/Hf5wH/139/ and https://jsfiddle.net/Tgm6Y/1/
+// I tried to implement it to my code but it use jquery so i decided not to
+
+
+const gui = document.querySelector('.sharpGui');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {  // adjust 300px to when you want it to start
+    gui.classList.add('scrolled');
+  } else {
+    gui.classList.remove('scrolled');
+  }
 });
+
